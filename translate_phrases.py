e720d7bff9da21fa1691a2b11d783356b626e69d
Blake Harris <blake@cktl.co>
2024-12-14 23:25:28 -0500

    Enhance multilingual translation script and instructions

--- translate_phrases.py
+++ translate_phrases.py
@@ -9,11 +9,73 @@ from utils import parse_phrase_inventory, CrewChiefAudioFile
 
 logging.basicConfig(level=logging.INFO)
 
+# Usage:
+# 1) Choose a capable multilingual LLM (such as qwen2.5:32B), install and start
+# the Ollama API server locally on all ip addresses (0.0.0.0)
+#
+#   $ OLLAMA_HOST=0.0.0.0:11434 ollama serve
+#
+#   (from another terminal window)
+#   $ ollama list
+#      NAME           ID              SIZE     MODIFIED
+#      qwen2.5:32b    9f13ba1299af    19 GB    7 weeks ago
+#
+# 2) Start the crew-chief-autovoicepack Docker container with params enabling
+# access to the host machine's network, ie:
+#
+#   $ docker run --rm -it --gpus all --add-host=host.docker.internal:host-gateway \
+#     --name crew-chief-autovoicepack \
+#     -v ~/crew-chief-autovoicepack/output:/app/output \
+#     -v ~/crew-chief-autovoicepack/my_recordings:/app/baseline \
+#     -v ./translated:/app/translated \
+#     ghcr.io/cktlco/crew-chief-autovoicepack:latest
+#
+#       ^^^ note the new 'translated' directory
+#
+#   # (optional) confirm the Ollama host is reachable
+#   $ telnet host.docker.internal 11434
+#   Trying X.X.X.X...
+#   Connected to host.docker.internal.
+#   Escape character is '^]'.  (Ctrl-C to exit)
+#
+# 3) At the `crew-chief-autovoicepack >` command line, run this translation script with the name of the target language and the path to the new phrase inventory CSV file the script will create:
+#
+#  python3 translate_phrases.py --target_language Portuguese --translated_phrase_inventory translated/phrase_inventory_pt.csv
+#
+#  ^^^ note the new 'xxx_pt.csv' file above where 'pt' is the language code for Portuguese
+#      this is not required, but helpful to keep things tidy with multiple languages
+#
+#    ... (example output)
+#    INFO:root:Translation 1: okay -> tá bom (is good)
+#    INFO:root:Translation 2: acknowledged -> confirmado (confirmed)
+#    INFO:root:Translation 3: acknowledged -> reconhecido (recognized)
+#    INFO:root:Translation 4: understood -> compreendido (understood)
+#    INFO:root:Translation 5: acknowledged -> reconhecido (recognized) ...
+
+# 4) After the translation script completes, you can find your new `phrase_inventory_XXX.csv` file in the `translated` directory on the host machine.
+#
+# 5) Manually review and edit the new CSV file if needed.
+#
+# 6) Create the voicepack using the normal instructions, making sure to reference the new
+#    phrase_inventory_XXX.csv file you created.
+#    - After translation, you may need to stop the Ollama server (ollama stop) to free up GPU RAM
+#    - Use a baseline recording voice in the target language for best results, although it will still work
+#      fine with the English baseline voices (ie, Luis)
+#
+#    python3 generate_voice_pack.py --your_name 'Ricardo' --voice_name 'Luis' --voice_name_tts 'Looees' --phrase_inventory translated/phrase_inventory_pt.csv --variation_count 1
+#
+#     [INFO] Starting...
+#     [INFO] Attribution file written to ./output/Luis/CREATED_BY.txt for voicepack 'Luis' version 20241215'.
+#     [INFO] Installation instructions written to ./output/Luis/INSTALLATION_INSTRUCTIONS.txt.
+#     [INFO] Considering phrase 2 - 'reconhecido' -> 'reconhecido'
+#     [INFO] Audio file created: ./output/Luis/voice/acknowledge/OK/5-a.wav
+#     [INFO] xtts_integrity validity check passed for ./output/Luis/voice/acknowledge/OK/5-a.wav with score 0.99
+#     ...
 
 def translate_phrase_ollama(
     input_phrase: str,
-    source_language_name: str = "English",
-    target_language_name: str = "German",
+    source_language_name: str,
+    target_language_name: str,
     ollama_host: str = "host.docker.internal:11434",
 ) -> Optional[str]:
     """
@@ -76,6 +138,7 @@ def main():
     )
     parser.add_argument(
         "--target_language",
+        default="German",
         help="The English name of the target language to translate to, i.e. 'German', 'Spanish', 'Japanese'. Feel free to experiment if there is a certain dialect or other nuance that you think would help, this wording is being fed directly to the LLM as part of the translation request.",
     )
     parser.add_argument(
@@ -136,7 +199,11 @@ def main():
             translated_phrase: Optional[str] = None
             num_retries = 0
             while translated_phrase is None and num_retries < args.max_retries:
-                translated_phrase = translate_phrase_ollama(english_phrase)
+                translated_phrase = translate_phrase_ollama(
+                    english_phrase,
+                    source_language_name="English",
+                    target_language_name=args.target_language,
+                )
                 if translated_phrase is None:
                     logging.error(
                         f"Failed to translate input phrase '{english_phrase}'. Retrying (#{num_retries}/{args.max_retries})."
@@ -150,7 +217,7 @@ def main():
                 (
                     translate_phrase_ollama(
                         translated_phrase,
-                        source_language_name="German",
+                        source_language_name=args.target_language,
                         target_language_name="English",
                     )
                     or "[Sanity check LLM call failed]"
